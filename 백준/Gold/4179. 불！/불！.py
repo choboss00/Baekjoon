fa171442d4dd1d162d638c18dbabfd4fa2c8328b
Jin__00 <anjdal64@gmail.com>
2023-07-31 19:55:48 +0900

    [Gold IV] Title: 불!, Time: 1380 ms, Memory: 73052 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.py
+++ 백준/Gold/4179. 불！/불！.py
@@ -1,66 +1,54 @@
 import sys
 from collections import deque
-
-input = sys.stdin.readline
-r, c = map(int, input().split())
-graph = []
-
-q_j = deque()
-q_f = deque()
-
-visited_j = [[0] * c for _ in range(r)]
-visited_f = [[0] * c for _ in range(r)]
-
-move = [
-    [1, 0],
-    [-1, 0],
-    [0, 1],
-    [0, -1]
-]
-
-for i in range(r):
-    temp = list(input())
-
-    for j in range(len(temp)):
-        if temp[j] == "J":
-            q_j.append((i, j))
-            visited_j[i][j] = 1
-
-        elif temp[j] == "F":
-            q_f.append((i, j))
-            visited_f[i][j] = 1
-
-    graph.append(temp)
-
-
 def bfs():
-    while q_f:
-        x, y = q_f.popleft()
+    while fire_queue:
+        x,y = fire_queue.popleft()
 
-        for i in range(4):
-            nx, ny = x + move[i][0], y + move[i][1]
+        for dx, dy in ((1,0), (-1,0), (0,1), (0,-1)):
+            nx = x + dx
+            ny = y + dy
 
-            if 0 <= nx < r and 0 <= ny < c:
-                if not visited_f[nx][ny] and graph[nx][ny] != "#":
-                    visited_f[nx][ny] = visited_f[x][y] + 1
-                    q_f.append((nx, ny))
+            if 0 <= nx < c and 0 <= ny < r:
+                if not fire[ny][nx] and board[ny][nx] != '#':
+                    fire[ny][nx] = fire[y][x] + 1
+                    fire_queue.append((nx,ny))
 
-    while q_j:
-        x, y = q_j.popleft()
+    while ji_queue:
+        x, y = ji_queue.popleft()
 
-        for i in range(4):
-            nx, ny = x + move[i][0], y + move[i][1]
+        for dx, dy in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+            nx = x + dx
+            ny = y + dy
 
-            if 0 <= nx < r and 0 <= ny < c:
-                if graph[nx][ny] != "#" and not visited_j[nx][ny]:
-                    if not visited_f[nx][ny] or visited_f[nx][ny] > visited_j[x][y] + 1:
-                        visited_j[nx][ny] = visited_j[x][y] + 1
-                        q_j.append((nx, ny))
-
+            if 0 <= nx < c and 0 <= ny < r:
+                if not ji[ny][nx] and board[ny][nx] != '#':
+                    if not fire[ny][nx] or fire[ny][nx] > ji[y][x] + 1:
+                        ji[ny][nx] = ji[y][x] + 1
+                        ji_queue.append((nx,ny))
             else:
-                return visited_j[x][y]
+                return ji[y][x]
 
     return "IMPOSSIBLE"
 
+input = sys.stdin.readline
+
+r,c = map(int, input().split())
+board = [list(input().strip()) for _ in range(r)]
+
+# 불의 위치, 지훈의 위치
+fire = [[0 for _ in range(c)] for _ in range(r)]
+ji = [[0 for _ in range(c)] for _ in range(r)]
+
+fire_queue = deque()
+ji_queue = deque()
+
+for y in range(r):
+    for x in range(c):
+        if board[y][x] == 'F':
+            fire[y][x] = 1
+            fire_queue.append((x,y))
+        if board[y][x] == 'J':
+            ji[y][x] = 1
+            ji_queue.append((x,y))
 
 print(bfs())
