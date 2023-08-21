06ef0ee86dc274be9c54dc695064e7db362699f6
Jin__00 <anjdal64@gmail.com>
2023-08-21 23:25:14 +0900

    [Gold III] Title: 줄 세우기, Time: 176 ms, Memory: 40640 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -1,38 +1,33 @@
 import sys
-from collections import deque
+sys.setrecursionlimit(100_000)
+def dfs(node):
+    # 방문한 경우
+    if visited[node]:
+        return
+
+    # 현 위치 방문 표시
+    visited[node] = True
+    # 다음 노드 탐색하기
+    for v in graph[node]:
+        dfs(v)
+    res.append(str(node))
 
 input = sys.stdin.readline
 
 n,m = map(int, input().split())
 
-# 위상 정렬
+# dfs
 graph = [[] for _ in range(n+1)]
-# 진입 차수
-indegree = [0] * (n+1)
 
 for _ in range(m):
     fr, se = map(int, input().split())
     # 방향이 있는 그래프
-    graph[fr].append(se)
-    indegree[se] += 1
+    graph[se].append(fr)
 
-queue = deque()
 res = []
+visited = [False] * (n+1)
 
 for i in range(1, n+1):
-    # 진입차수가 0 인 원소를 큐에 담고
-    if indegree[i] == 0:
-        queue.append(i)
-# 큐의 원소를 빼고
-while queue:
-    cur = queue.popleft()
-    # 출력할 리스트에 담고 ( 이미 사용한 원소니 )
-    res.append(cur)
+    dfs(i)
 
-    for i in graph[cur]:
-        indegree[i] -= 1
-
-        if indegree[i] == 0:
-            queue.append(i)
-
-print(*res)
+print(' '.join(res))
