ab291ce48d8c6be12132f6de04f175260737818b
Jin__00 <anjdal64@gmail.com>
2024-05-29 12:46:50 +0900

    [level 1] Title: 삼총사, Time: 4.32 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -9,7 +9,7 @@ class Solution {
     
     static int length;
     
-    static void dfs(int[] number) {
+    static void dfs(int[] number, int depth) {
         
         if (stack.size() == 3) {
             int s = stack.stream().mapToInt(Integer::intValue).sum();
@@ -21,12 +21,12 @@ class Solution {
             
         }
         
-        for(int i = 0; i < length; i++) {
+        for(int i = depth; i < length; i++) {
             if (!visited[i]) {
                 visited[i] = true;
                 stack.add(number[i]);
                 
-                dfs(number);
+                dfs(number, i+1);
                 
                 visited[i] = false;
                 stack.pollLast();
@@ -39,8 +39,8 @@ class Solution {
         
         visited = new boolean[length];
         
-        dfs(number);
+        dfs(number, 0);
         
-        return answer / 6;
+        return answer;
     }
 }
